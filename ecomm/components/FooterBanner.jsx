f3b7d8e42a42577f7592f4c8f9cca0174faf95ee
KakwiriStephen <ngottakakwiri@gmail.com>
2023-01-10 22:05:42 +0300

    Footer banner

--- ecomm/components/FooterBanner.jsx
+++ ecomm/components/FooterBanner.jsx
@@ -2,29 +2,38 @@ import React from "react";
 import Link from "next/link";
 import { urlFor } from "../LIB/client";
 
-const FooterBanner = ({ footerBanner }) => {
+const FooterBanner = ({
+  footerBanner: {
+    discount,
+    largeText1,
+    largeText2,
+    saleTime,
+    smallText,
+    midText,
+    desc,
+    product,
+    image,
+    buttonText,
+  },
+}) => {
   return (
     <div className="footer-banner-container">
       <div className="banner-desc">
         <div className="left">
-          <p>{footerBanner.discount}</p>
-          <h3>{footerBanner.largeText1}</h3>
-          <h3>{footerBanner.largeText2}</h3>
-          <p>{footerBanner.saleTime}</p>
+          <p>{discount}</p>
+          <h3>{largeText1}</h3>
+          <h3>{largeText2}</h3>
+          <p>{saleTime}</p>
         </div>
         <div className="right">
-          <p>{footerBanner.smallText}</p>
-          <h3>{footerBanner.midText}</h3>
-          <p>{footerBanner.desc}</p>
-          <Link href={`/product/${footerBanner.product}`}>
-            <button type="button">{footerBanner.buttonText}</button>
+          <p>{smallText}</p>
+          <h3>{midText}</h3>
+          <p>{desc}</p>
+          <Link href={`/product/${product}`}>
+            <button type="button">{buttonText}</button>
           </Link>
         </div>
-        <img
-          src={urlFor(footerBanner.image)}
-          alt=""
-          className="footer-banner-image"
-        />
+        <img src={urlFor(image)} alt="" className="footer-banner-image" />
       </div>
     </div>
   );
