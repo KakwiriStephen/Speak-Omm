11c67008458e1542312f3b80138193176a063954
KakwiriStephen <ngottakakwiri@gmail.com>
2023-01-10 22:02:51 +0300

    Footer banner

--- ecomm/components/FooterBanner.jsx
+++ ecomm/components/FooterBanner.jsx
@@ -1,7 +1,33 @@
 import React from "react";
+import Link from "next/link";
+import { urlFor } from "../LIB/client";
 
-const FooterBanner = () => {
-  return <div>FooterBanner</div>;
+const FooterBanner = ({ footerBanner }) => {
+  return (
+    <div className="footer-banner-container">
+      <div className="banner-desc">
+        <div className="left">
+          <p>{footerBanner.discount}</p>
+          <h3>{footerBanner.largeText1}</h3>
+          <h3>{footerBanner.largeText2}</h3>
+          <p>{footerBanner.saleTime}</p>
+        </div>
+        <div className="right">
+          <p>{footerBanner.smallText}</p>
+          <h3>{footerBanner.midText}</h3>
+          <p>{footerBanner.desc}</p>
+          <Link href={`/product/${footerBanner.product}`}>
+            <button type="button">{footerBanner.buttonText}</button>
+          </Link>
+        </div>
+        <img
+          src={urlFor(footerBanner.image)}
+          alt=""
+          className="footer-banner-image"
+        />
+      </div>
+    </div>
+  );
 };
 
 export default FooterBanner;
