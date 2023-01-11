71270037c8cb3259086300bc0462919058875462
KakwiriStephen <ngottakakwiri@gmail.com>
2023-01-11 12:59:50 +0300

    Product page

--- ecomm/pages/product/[slug].js
+++ ecomm/pages/product/[slug].js
@@ -1,7 +1,18 @@
 import React from "react";
+import { urlFor, client } from "../../LIB/client";
 
 const ProductDetails = () => {
-  return <div>Product</div>;
+  return (
+    <div>
+      <div className="product-detail-container">
+        <div>
+          <div className="image-container">
+            <img src="" alt="" />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default ProductDetails;
