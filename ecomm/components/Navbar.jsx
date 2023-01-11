5726cfb2df182b9889194a4ccbf3411068fa8d6c
KakwiriStephen <ngottakakwiri@gmail.com>
2023-01-11 11:31:18 +0300

    Navbar

--- ecomm/components/Navbar.jsx
+++ ecomm/components/Navbar.jsx
@@ -1,7 +1,15 @@
 import React from "react";
+import Link from "next/link";
+import { AiOutlineShopping } from "react-icons/ai";
 
 const Navbar = () => {
-  return <div>Navbar</div>;
+  return (
+    <div className="navbar-container">
+      <p className="logo">
+        <Link href="/">Sounds Store</Link>
+      </p>
+    </div>
+  );
 };
 
 export default Navbar;
