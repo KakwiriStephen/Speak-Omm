268d2e747d00577611708b111939d6b2153a3dc5
KakwiriStephen <ngottakakwiri@gmail.com>
2023-01-11 12:11:16 +0300

    NavBar

--- ecomm/components/Navbar.jsx
+++ ecomm/components/Navbar.jsx
@@ -1,12 +1,13 @@
 import React from "react";
 import Link from "next/link";
 import { AiOutlineShopping } from "react-icons/ai";
+import { BsSoundwave } from "react-icons/bs";
 
 const Navbar = () => {
   return (
     <div className="navbar-container">
       <p className="logo">
-        <Link href="/">Sounds Store</Link>
+        <Link href="/">{<BsSoundwave />}Sounds Store</Link>
       </p>
 
       <button type="button" className="cart-icon" onClick="">
