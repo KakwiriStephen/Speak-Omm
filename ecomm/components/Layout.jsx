fa016f56a2ccaeb1057e6ca8407ee9910dd44e9e
KakwiriStephen <ngottakakwiri@gmail.com>
2023-01-11 11:11:49 +0300

    Layout

--- ecomm/components/Layout.jsx
+++ ecomm/components/Layout.jsx
@@ -1,7 +1,23 @@
 import React from "react";
+import Head from "next/head";
+import Navbar from "./Navbar";
+import Footer from "./Footer";
 
 const Layout = () => {
-  return <div>Layout</div>;
+  return (
+    <div className="layout">
+      <Head>
+        <title>Sounds Store</title>
+      </Head>
+      <header>
+        <Navbar />
+      </header>
+      <main className="main-container">Empty</main>
+      <footer>
+        <Footer />
+      </footer>
+    </div>
+  );
 };
 
 export default Layout;
