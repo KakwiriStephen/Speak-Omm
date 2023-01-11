a6ba0f26aa36d2c5885e48a6e7e3c02e63cc27b4
KakwiriStephen <ngottakakwiri@gmail.com>
2023-01-11 13:18:37 +0300

    Products page

--- ecomm/pages/product/[slug].js
+++ ecomm/pages/product/[slug].js
@@ -1,7 +1,8 @@
 import React from "react";
 import { urlFor, client } from "../../LIB/client";
 
-const ProductDetails = () => {
+const ProductDetails = (product, products) => {
+  const { image, name, details, price } = product;
   return (
     <div>
       <div className="product-detail-container">
@@ -13,6 +14,40 @@ const ProductDetails = () => {
       </div>
     </div>
   );
+};
+
+export const getStaticPaths = async () => {
+  const query = `*[_type == "product"] {
+      slug {
+        current
+      }
+    }
+    `;
+
+  const products = await client.fetch(query);
+
+  const paths = products.map((product) => ({
+    params: {
+      slug: product.slug.current,
+    },
+  }));
+
+  return {
+    paths,
+    fallback: "blocking",
+  };
+};
+
+export const getStaticProps = async ({ params: { slug } }) => {
+  const query = `*[_type == "product" && slug.current == '${slug}'][0]`;
+  const productsQuery = '*[_type == "product"]';
+
+  const product = await client.fetch(query);
+  const products = await client.fetch(productsQuery);
+
+  return {
+    props: { products, product },
+  };
 };
 
 export default ProductDetails;
