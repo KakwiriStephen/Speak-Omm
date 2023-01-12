0a87233309443d86590e3d8040ea0553b6f8268d
KakwiriStephen <ngottakakwiri@gmail.com>
2023-01-12 13:20:38 +0300

    Cart

--- ecomm/components/Cart.jsx
+++ ecomm/components/Cart.jsx
@@ -10,10 +10,27 @@ import { TiDeleteOutline } from "react-icons/ti";
 import toast from "react-hot-toast";
 
 import { useStateContext } from "../context/StateContext";
-import { urlFor } from "../lib/client";
+import { urlFor } from "../LIB/client";
 
 const Cart = () => {
-  return <div>Cart</div>;
+  const cartRef = useRef();
+  const { totalPrice, totalQuantities, CartItems, setShowCart } =
+    useStateContext();
+  return (
+    <div className="cart-wrapper" ref={cartRef}>
+      <div className="cart-container">
+        <button
+          type="button"
+          className="cart-heading"
+          onClick={() => setShowCart(false)}
+        >
+          <AiOutlineLeft />
+          <span className="heading">Your Cart</span>
+          <span className="cart-num-items">({totalQuantities} Items)</span>
+        </button>
+      </div>
+    </div>
+  );
 };
 
 export default Cart;
