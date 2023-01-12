9aee9bd1a6f27d6fba353573c36a3aeac36538de
KakwiriStephen <ngottakakwiri@gmail.com>
2023-01-12 10:59:03 +0300

    QUantity State

--- ecomm/context/stateContext.js
+++ ecomm/context/stateContext.js
@@ -10,6 +10,17 @@ export const StateContext = ({ children }) => {
   const [totalQuantities, setTotalQuantities] = useState();
   const [qty, setQty] = useState(1);
 
+  const incQty = () => {
+    setQty((prevQty) => prevQty + 1);
+  };
+
+  const decQty = () => {
+    setQty((prevQty) => {
+      if (prevQty - 1 < 1) return 1;
+
+      return prevQty - 1;
+    });
+  };
   return (
     <Context.Provider
       value={{
