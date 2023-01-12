20df933502a391ac1ed65680e63e07fd3862dfcd
KakwiriStephen <ngottakakwiri@gmail.com>
2023-01-12 10:28:55 +0300

    Contex States

--- ecomm/context/stateContext.js
+++ ecomm/context/stateContext.js
@@ -1,6 +1,14 @@
 import React, { createContext, useContext, useState, useEffect } from "react";
 import { toast } from "react-hot-toast";
 
-const Contex = createContext();
+const Context = createContext();
 
-export const StateContext = ({ children }) => {};
+export const StateContext = ({ children }) => {
+  const [showCart, setShowCart] = useState(false);
+  const [cartItems, setCartItems] = useState();
+  const [totalPrice, setTotalPrice] = useState();
+  const [totalQuantities, setTotalQuantities] = useState();
+  const [qty, setQty] = useState(1);
+
+  return <Context.Provider value={{}}>{children}</Context.Provider>;
+};
