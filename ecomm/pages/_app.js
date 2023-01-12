b47b257b9ad1c6c95601ecc8e5b3fb3c5769abd7
KakwiriStephen <ngottakakwiri@gmail.com>
2023-01-12 10:53:24 +0300

    Product details logig

--- ecomm/pages/_app.js
+++ ecomm/pages/_app.js
@@ -1,5 +1,6 @@
 import React from "react";
 import "../styles/globals.css";
+import { Toaster } from "react-hot-toast";
 import { Layout } from "../components";
 
 import { StateContext } from "../context/StateContext";
@@ -8,6 +9,7 @@ export default function App({ Component, pageProps }) {
   return (
     <StateContext>
       <Layout>
+        <Toaster />
         <Component {...pageProps} />
       </Layout>
     </StateContext>
