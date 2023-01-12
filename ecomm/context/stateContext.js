45548c3278d0ac3efea72a77ae6270dca8724fda
KakwiriStephen <ngottakakwiri@gmail.com>
2023-01-12 11:23:03 +0300

    Add to cart

--- ecomm/context/stateContext.js
+++ ecomm/context/stateContext.js
@@ -5,7 +5,7 @@ const Context = createContext();
 
 export const StateContext = ({ children }) => {
   const [showCart, setShowCart] = useState(false);
-  const [cartItems, setCartItems] = useState();
+  const [cartItems, setCartItems] = useState([]);
   const [totalPrice, setTotalPrice] = useState();
   const [totalQuantities, setTotalQuantities] = useState();
   const [qty, setQty] = useState(1);
