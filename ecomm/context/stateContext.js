79ba58f3589d9641be7b431d8ea4ed7916e000b8
KakwiriStephen <ngottakakwiri@gmail.com>
2023-01-12 10:21:14 +0300

    React-Context

--- ecomm/context/stateContext.js
+++ ecomm/context/stateContext.js
@@ -1,2 +1,6 @@
 import React, { createContext, useContext, useState, useEffect } from "react";
 import { toast } from "react-hot-toast";
+
+const Contex = createContext();
+
+export const StateContext = ({ children }) => {};
