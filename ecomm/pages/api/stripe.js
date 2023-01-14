9a9ade3202019226b5ca68cf38586bdabf556cf6
KakwiriStephen <ngottakakwiri@gmail.com>
2023-01-14 10:55:13 +0300

    Bug Hunting

--- ecomm/pages/api/stripe.js
+++ ecomm/pages/api/stripe.js
@@ -10,10 +10,7 @@ export default async function handler(req, res) {
         mode: "payment",
         payment_method_types: ["card"],
         billing_address_collection: "auto",
-        shipping_options: [
-          { shipping_rate: "shr_1MPiV6Hp6QC0RzA7i5LW1rD4" },
-          { shipping_rate: "shr_1MPiWoHp6QC0RzA7LFiefKvC" },
-        ],
+        shipping_options: [{ shipping_rate: "shr_1MQ4VCDdbJuWh1eO2tL2ej2K" }],
         line_items: req.body.map((item) => {
           const img = item.image[0].asset._ref;
           const newImage = img
