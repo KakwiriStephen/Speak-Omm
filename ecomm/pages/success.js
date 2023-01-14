e59d53b9ccca7681314df2d1b64c2407a31c167f
KakwiriStephen <ngottakakwiri@gmail.com>
2023-01-14 12:34:12 +0300

    Success Page

--- ecomm/pages/success.js
+++ ecomm/pages/success.js
@@ -1,4 +1,8 @@
 import React from "react";
+import { useState, useEffect } from "react";
+import { BsBagCheckFill } from "react-icons/bs";
+import Link from "next/link";
+import { useStateContext } from "../context/StateContext";
 
 const Success = () => {
   return <div>success</div>;
