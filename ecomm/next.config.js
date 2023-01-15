44b3b72a73109838eafcebd38d2962d969fe8434
KakwiriStephen <ngottakakwiri@gmail.com>
2023-01-15 10:44:40 +0300

    Deploying

--- ecomm/next.config.js
+++ ecomm/next.config.js
@@ -1,6 +1,12 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-};
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//   reactStrictMode: true,
+// };
 
-module.exports = nextConfig;
+// module.exports = nextConfig;
+
+module.exports = {
+  experimental: {
+    forceSwcTransforms: true,
+  },
+};
